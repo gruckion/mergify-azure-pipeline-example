d59b07d82adb7ad4dcbc6ff85742296853597c58
Stephen Rayner <srayner@neworbit.co.uk>
2019-05-23 11:15:26 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mergify_azure_pipeline_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
